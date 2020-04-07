ea5465caf2d623569e222ef68d00c4a8ad9b3481
yangq <yangq0979@gmail.com>
2020-04-07 18:19:20 +0800

    配置 react 在 typescript 文件中使用
    在 .tsx 文件中使用 react, react-dom, 需要先安装类型声明文件
    yarn add @types/react --dev
    yarn add @types/react-dom --dev
    
    另注：
    第一次使用的 ts-loader, webpack 打包一直报错，后来换成了 awesome-typescript-loader

---
+++ lib/button.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function Button() {
+    return (
+        <div>这是一个 button</div>
+    );
+}
+
+export default Button;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'hooks-ui',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'hooks-ui',
+            template: 'index.html'
+        })
+    ]
+};
