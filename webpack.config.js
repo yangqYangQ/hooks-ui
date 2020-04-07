748fee99d251184dbd7e711e1090c78437c4d46b
yangq <yangq0979@gmail.com>
2020-04-07 21:55:55 +0800

    不打包 react, react-dom
    配置 webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,21 @@ module.exports = {
             title: 'hooks-ui',
             template: 'index.html'
         })
-    ]
+    ],
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+            umd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom'
+        }
+    }
 };
